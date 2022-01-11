5a8991e3ac1821c3f9d2ffe72816a6e454c40d8b
Mahmud Sajjad Abeer <mahmudsajjad.abeer@gmail.com>
2022-01-11 18:37:08 +0600

    BugFix: Auto checkin checkout fix

--- db_ops/pymongo_db_config.py
+++ db_ops/pymongo_db_config.py
@@ -1,8 +1,10 @@
 import sys
 from pymongo import MongoClient
+
 sys.path.append("..")
 
 from env_vars import MONGO_CERT_PATH
+
 
 class SingletonDB:
     _fsdb = None
@@ -10,9 +12,7 @@ class SingletonDB:
     def __init__(self, *args, **kwargs):
         if not self._fsdb:
             uri = "mongodb+srv://pslbash.t89vp.mongodb.net/myFirstDatabase?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority"
-            client = MongoClient(
-                uri, tls=True, tlsCertificateKeyFile=MONGO_CERT_PATH
-            )
+            client = MongoClient(uri, tls=True, tlsCertificateKeyFile=MONGO_CERT_PATH)
 
             self._fsdb = client["pslbash"]
 
--- env_vars.py
+++ env_vars.py
@@ -6,5 +6,5 @@ load_dotenv()
 
 DEFAULT_CHANNEL = int(os.getenv("DEFAULT_CHANNEL"))
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
-EN_KEY = os.getenv('EN_KEY')
-MONGO_CERT_PATH = str(os.getenv('MONGO_CERT_PATH'))
+EN_KEY = os.getenv("EN_KEY")
+MONGO_CERT_PATH = str(os.getenv("MONGO_CERT_PATH"))
